a17a22c01987ccde8c57f31ef8d7abc27de88515
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-24 01:29:46 +0530

    bug fixing

--- user-router.js
+++ user-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./data/db');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { 
     const userInformation = req.body;
 
     if (userInformation.title && userInformation.contents) {
