da12cc78f25de812ea138afa4a7a333bf114a95c
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-24 02:48:59 +0530

    Completed stretch goal of creating react app

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./user-router');
 const server = express();
 
 server.use(express.json());
-
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send("it's working")
